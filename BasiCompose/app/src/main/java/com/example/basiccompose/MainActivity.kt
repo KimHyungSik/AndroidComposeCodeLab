92026eab6a8375f66b5da8b08f9d6542cbe186ac
KimHyungSik <bagite369@naver.com>
2022-08-02 17:14:16 +0900

    Finish Basic Compose code lab

--- BasiCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ BasiCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.basiccompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +35,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                onClick = onContinueClicked,
+                modifier = Modifier.padding(vertical = 24.dp)
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable {
+        mutableStateOf(true)
+    }
+    if(shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    }else{
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = List(100){"$it"}){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names){ name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(targetValue = if (expanded.value) 48.dp else 0.dp)
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicComposeTheme {
-        Greeting("Android")
+        OnboardingScreen {}
     }
 }
--- BasiCompose/app/src/main/java/com/example/basiccompose/ui/theme/Theme.kt
+++ BasiCompose/app/src/main/java/com/example/basiccompose/ui/theme/Theme.kt
@@ -5,38 +5,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 )
 
 @Composable
 fun BasicComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
